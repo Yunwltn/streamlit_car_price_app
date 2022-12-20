1db3718f0280ad49443662dbfa57c48cc63e072b
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2022-12-20 11:48:13 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def run_home_app() :
-    st.text('안녕하세요')
+    st.text('환영합니다^^')
     st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다')
     st.text('데이터 분석 및 고객 정보를 넣으면 얼마정도의 차를 구매할지를 예측해줍니다')
 
